c21b55a3d414bdda4816610511e94f7423f2bc2e
Nekesa <c.nekesa@alustudent.com>
2024-07-24 17:29:17 +0300

    changing ranges in main.py

--- summative/API/main.py
+++ summative/API/main.py
@@ -31,14 +31,14 @@ app = FastAPI()
 
 # create a pedantic class for the request
 class WineQRequest(BaseModel):
-    fixed_acidity: float = Field(gt=0, lt=50.0)
-    volatile_acidity: float = Field(gt=0, lt=5.000)
-    residual_sugar: float = Field(gt=0, lt=50.0)
-    chlorides: float = Field(gt=0, lt=5.000)
-    free_SO2: float = Field(gt=0, lt=100.0)
-    sulphates: float = Field(gt=0, lt=5.000)
-    alcohol: float = Field(gt=0, lt=50.0)
-    colour: int = Field(gt=0, lt=1)
+    fixed_acidity: float = Field(gt=0, lt=1000.0)
+    volatile_acidity: float = Field(gt=0, lt=1000.000)
+    residual_sugar: float = Field(gt=0, lt=1000.0)
+    chlorides: float = Field(gt=0, lt=1000.000)
+    free_SO2: float = Field(gt=0, lt=1000.0)
+    sulphates: float = Field(gt=0, lt=1000.000)
+    alcohol: float = Field(gt=0, lt=1000.0)
+    colour: int = Field(gt=0, lt=2)
 
 
 # class testing 
@@ -58,7 +58,8 @@ async def make_prediction(wineq_request: WineQRequest):
         #new_data_scaled = scaler.transform(single_row)
         #new_value = model.predict(new_data_scaled)
         new_value = model.predict(single_row)
-        return new_value[0]
+        integer_quality = int(new_value[0])  # Convert the predicted value to an integer
+        return {"predicted Quality": integer_quality}
         #return {"predicted Quality ": new_value[0][0]}
         
     #except:
