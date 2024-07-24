6004fbe01c37c72e31b6b270e4fa847712c63f0a
Nekesa <c.nekesa@alustudent.com>
2024-07-24 17:11:26 +0300

    main.py changing attributes and range

--- summative/API/main.py
+++ summative/API/main.py
@@ -6,35 +6,41 @@ from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
 
 
-import pickle as pk
+# import pickle as pk
 
 # load the model in the env 
 # Opening saved model
-with open("./API/model.pkl", "rb") as file:
-    model = pk.load(file)
+#with open("../API/model.pkl", "rb") as file:
+# model = pk.load(file)
+
+import joblib
+
+model = joblib.load("model.pkl")
 
 # create app instance 
-app = FastAPI()
+app = FastAPI() 
 
 # Configure CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow requests from any origin
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],  # Allow GET and POST requests
-    allow_headers=["*"],  # Allow any headers
-)
+#app.add_middleware(
+#  CORSMiddleware,
+#   allow_origins=["*"],  # Allow requests from any origin
+#   allow_credentials=True,
+#    allow_methods=["GET", "POST"],  # Allow GET and POST requests
+#  allow_headers=["*"],  # Allow any headers
+#)
 
 # create a pedantic class for the request
 class WineQRequest(BaseModel):
-    fixed_acidity: float = Field(gt=0, lt=10000)
-    volatile_acidity: float = Field(gt=0, lt=10000)
-    residual_sugar: float = Field(gt=0, lt=10000)
-    chlorides: float = Field(gt=0, lt=10000)
-    total_SO2: float = Field(gt=0, lt=10000)
-    sulphates: float = Field(gt=0, lt=10000)
-    alcohol: float = Field(gt=0, lt=10000)
-    
+    fixed_acidity: float = Field(gt=0, lt=50.0)
+    volatile_acidity: float = Field(gt=0, lt=5.000)
+    residual_sugar: float = Field(gt=0, lt=50.0)
+    chlorides: float = Field(gt=0, lt=5.000)
+    free_SO2: float = Field(gt=0, lt=100.0)
+    sulphates: float = Field(gt=0, lt=5.000)
+    alcohol: float = Field(gt=0, lt=50.0)
+    colour: int = Field(gt=0, lt=1)
+
+
 # class testing 
 @app.get("/class")
 async def get_greet():
@@ -48,12 +54,15 @@ async def get_hello():
 @app.post('/predict', status_code=status.HTTP_200_OK)
 async def make_prediction(wineq_request: WineQRequest):
     try:
-        single_row = [[wineq_request.fixed_acidity, wineq_request.volatile_acidity, wineq_request.residual_sugar, wineq_request.chlorides, wineq_request.total_SO2, wineq_request.sulphates, wineq_request.alcohol]]
-        new_data_scaled = scaler.transform(single_row)
-        new_value = model.predict(new_data_scaled)
-        return {"predicted Quality ": new_value[0]}
-    except:
-        raise HTTPException(status_code=500, detail="Something went wrong.")
+        single_row = [[wineq_request.fixed_acidity, wineq_request.volatile_acidity, wineq_request.residual_sugar, wineq_request.chlorides, wineq_request.free_SO2, wineq_request.sulphates, wineq_request.alcohol, wineq_request.colour]]
+        #new_data_scaled = scaler.transform(single_row)
+        #new_value = model.predict(new_data_scaled)
+        new_value = model.predict(single_row)
+        return new_value[0]
+        #return {"predicted Quality ": new_value[0][0]}
+        
+    #except:
+    #    raise HTTPException(status_code=500, detail="Something went wrong.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
